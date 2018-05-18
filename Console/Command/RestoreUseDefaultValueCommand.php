70f007c8adf40efd1eb35183f008c2eceaee2354
Jordan Victor Schinella <jvschin@gmail.com>
2018-05-18 15:35:47 -0400

    Update RestoreUseDefaultValueCommand.php

--- Console/Command/RestoreUseDefaultValueCommand.php
+++ Console/Command/RestoreUseDefaultValueCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace Experius\Cleanup\Console\Command;
+namespace Jordanvector\Cleanup\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
